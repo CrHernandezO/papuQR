c709ee9f224541536cbab4da2a1723638eb79f23
Happy <cr.hernandezo@duocuc.cl>
2024-09-13 20:53:37 -0300

    v4.2 pb

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor(  private animationController: AnimationController
+  ) { }
 
   ngOnInit() {
   }
 
+  ngAfterContentInit() {
+    this.animarLogin();
+  }
+
+  animarLogin() {
+    const loginIcon = document.querySelector('.login img') as HTMLElement;
+
+    const animacion = this.animationController
+      .create()
+      .addElement(loginIcon)
+      .duration(4000) // Ajusta la duración de la animación
+      .iterations(Infinity)
+      .keyframes([
+        { offset: 0, transform: 'translateX(-100px)' },
+        { offset: 0.5, transform: 'translateX(100px)' },
+        { offset: 1, transform: 'translateX(-100px)' },
+      ]);
+
+    animacion.play();
+  }
 }
